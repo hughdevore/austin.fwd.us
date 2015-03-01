f1f211b60de23cba907ee5ca5f25d1e339949742
Gabe Rubio <gtrubio192@gmail.com>
2015-03-01 12:30:14 -0600

    Added responsive features to page. Page layout is in order, twitter feed established, money counter is functional with corrected maths

--- scripts/app.controllers.js
+++ scripts/app.controllers.js
@@ -1,14 +1,11 @@
 angular.module('app.controllers', [])
-.controller('homeCtrl', function($scope, $state) {
-	$scope.goHome = function() {
-		$state.go('home');
-	};
+.controller('homeCtrl', function($scope, $state,$interval) {
     
      $scope.intcomma = function(number) { return parseInt(number, 10).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); };
 
      $scope.now = Math.round(new Date().getTime()/1000.0)
 
-     $scope.prevTime = new Date('Jun 28, 2013, 00:00:00');
+     $scope.prevTime = new Date('Dec 3 2014, 00:00:00');
      $scope.thisTime = new Date();
      $scope.diff = $scope.thisTime.getTime() - $scope.prevTime.getTime();
      $scope.seconds = ($scope.diff/ (1000));
@@ -21,18 +18,23 @@ angular.module('app.controllers', [])
      $scope.numminutes = Math.floor(((($scope.seconds % 31554000000) % 86400) % 3600) / 60);
      $scope.numseconds = Math.floor(((($scope.seconds % 31554000000) % 86400) % 3600));
 
-     $scope.MSrate = .42808333;
+     $scope.MSrate = .002143582;
      $scope.increment = ($scope.MSrate*100);
      $scope.total = ($scope.diff*$scope.MSrate);
-     $scope.commatotal = $scope.intcomma(total);
+     $scope.commatotal = $scope.intcomma($scope.total);
 
 //     $scope.container = "hereeerrrreeeee";
-     setInterval(function () {
-         $scope.total+= $scope.increment;
+     $interval(function () {
+         $scope.total += $scope.increment;
          $scope.container = $scope.intcomma($scope.total);
- //        container.innerHTML = "<h2>$" + intcomma(total) + "</h2>";
+         console.log($scope.increment);
      }, 100);
-
+    
+    $scope.goHome = function() {
+		$state.go('home');
+	};
+    
+//    $scope.container = 'test';
 })
 .controller('tweetCtrl', function($scope, $state) {
 	$scope.goTweet = function() {

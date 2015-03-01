b0d784f927a212e68a05fd9462777dc09c76e130
Gabe Rubio <gtrubio192@gmail.com>
2015-02-28 21:30:02 -0600

    fixing counter, still broke but uploading so Hugh can check it

--- scripts/app.controllers.js
+++ scripts/app.controllers.js
@@ -4,35 +4,34 @@ angular.module('app.controllers', [])
 		$state.go('home');
 	};
     
-//     intcomma = function(number) { return parseInt(number, 10).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); };
+     $scope.intcomma = function(number) { return parseInt(number, 10).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); };
 
-//     var now = Math.round(new Date().getTime()/1000.0)
+     $scope.now = Math.round(new Date().getTime()/1000.0)
 
-//     var prevTime = new Date('Jun 28, 2013, 00:00:00');
-//     var thisTime = new Date();
-//     var diff = thisTime.getTime() - prevTime.getTime();
-//     var seconds = (diff/ (1000));
+     $scope.prevTime = new Date('Jun 28, 2013, 00:00:00');
+     $scope.thisTime = new Date();
+     $scope.diff = $scope.thisTime.getTime() - $scope.prevTime.getTime();
+     $scope.seconds = ($scope.diff/ (1000));
 
-//     // this is for the text part
-//     var numdays = Math.floor((seconds % 31554000000) / 86400); 
+     // this is for the text part
+     $scope.numdays = Math.floor(($scope.seconds % 31554000000) / 86400); 
 
-//     var numhours = Math.floor(((seconds % 31554000000) % 86400) / 3600);
+     $scope.numhours = Math.floor((($scope.seconds % 31554000000) % 86400) / 3600);
 
-//     var numminutes = Math.floor((((seconds % 31554000000) % 86400) % 3600) / 60);
+     $scope.numminutes = Math.floor(((($scope.seconds % 31554000000) % 86400) % 3600) / 60);
+     $scope.numseconds = Math.floor(((($scope.seconds % 31554000000) % 86400) % 3600));
 
-//     var numseconds = Math.floor((((seconds % 31554000000) % 86400) % 3600));
-
-//     var MSrate = .42808333;
-//     var increment = (MSrate*100);
-//     var total = (diff*MSrate);
-//     var commatotal = intcomma(total);
+     $scope.MSrate = .42808333;
+     $scope.increment = ($scope.MSrate*100);
+     $scope.total = ($scope.diff*$scope.MSrate);
+     $scope.commatotal = $scope.intcomma(total);
 
 //     $scope.container = "hereeerrrreeeee";
-//     setInterval(function () {
-//         total+= increment;
-// //        $scope.container = intcomma(total);
-// //        container.innerHTML = "<h2>$" + intcomma(total) + "</h2>";
-//     }, 100);
+     setInterval(function () {
+         $scope.total+= $scope.increment;
+         $scope.container = $scope.intcomma($scope.total);
+ //        container.innerHTML = "<h2>$" + intcomma(total) + "</h2>";
+     }, 100);
 
 })
 .controller('tweetCtrl', function($scope, $state) {
